486ada811dabc5068554a1fb3c45b712c6e9f069
daredevilek <daredevilek@gmail.com>
2018-03-01 14:00:42 +0100

    layout translations on server side

--- server.R
+++ server.R
@@ -29,8 +29,8 @@ shinyServer(function(input, output, session) {
   smpF <- function(){
    infRa2 <- seq(input$mmInf[1], input$mmInf[2], 0.0005)
    sampleN2 <- (log(1-input$probaB)/log(1-infRa2*input$czTn))
-   saN2 <- as.data.frame(cbind('N' = sampleN2, 'infekcje' = infRa2))
-   tableSMP <- DT::datatable(data.frame('Częstość infekcji' = infRa2,
+   saN2 <- as.data.frame(cbind('N' = sampleN2, 'infections' = infRa2))
+   tableSMP <- DT::datatable(data.frame('Infections rate' = infRa2,
                                         'N' = ceiling(sampleN2)),
                              class = 'table-condensed stripe hover dt[-head|-body]-left order-column',
                              rownames = F, style = 'bootstrap', selection = 'none',
@@ -47,9 +47,9 @@ shinyServer(function(input, output, session) {
   rateiF <- function() {
   samN3 <- seq(1, input$sampleN3)
   infRa3 <- ((1-(nthroot((1-input$probaB3), samN3)))*1000)/input$czTg
-  iR3 <- as.data.frame(cbind('Maks.L.Infekcji' = infRa3, 'N' = samN3))
+  iR3 <- as.data.frame(cbind('Max.N.Infections' = infRa3, 'N' = samN3))
   tableG <- DT::datatable(data.frame('N' = samN3,
-                                     'Graniczny poziom infekcji' = floor(infRa3)),
+                                     'Upper bound of infections' = floor(infRa3)),
                           class = 'table-condensed stripe hover order-column',
                           rownames = F, style = 'bootstrap', selection = 'none',
                           options = list(columnDefs =
@@ -67,33 +67,33 @@ shinyServer(function(input, output, session) {
       theGraphP <- plotDFP %>% plot_ly(x = ~N,
                                        hoverinfo = 'text', text = hovertxtp) %>%
                                add_lines(y = ~p, color = I("#e74b47")) %>%          
-                               layout(xaxis = list(title = 'Liczebność próby'),
-                               yaxis = list(title = 'Prawdopodobieństwo wykrycia',
+                               layout(xaxis = list(title = 'Sample size'),
+                               yaxis = list(title = 'Probability of detection',
                                             tickangle = -30))
   })
   
   output$wykresN <- renderPlotly({
       plotDFN <- data.frame(smpF()$saN)
       hovertxtn <- paste("N: ", ceiling(plotDFN$N), "<br>",
-                         "infekcje: ", round(plotDFN$infekcje, digits = 4))
-      theGraphN <- plotDFN %>% plot_ly(x = ~infekcje,
+                         "infections: ", round(plotDFN$infections, digits = 4))
+      theGraphN <- plotDFN %>% plot_ly(x = ~infections,
                                        hoverinfo = 'text', text = hovertxtn) %>% 
                                add_lines(y=~N, color = I("#e74b47")) %>%          
-                               layout(xaxis = list(title = 'Poziom infekcji'),
-                                      yaxis = list(title = 'Liczebność próby',
+                               layout(xaxis = list(title = 'Infections rate'),
+                                      yaxis = list(title = 'Sample size',
                                                    tickangle = -30))
       })
   
   output$wykresG <- renderPlotly({
       
       plotDFG <- data.frame(rateiF()$iR)
-      hovertxtg <- paste("infekcje: ", round(plotDFG$Maks.L.Infekcji, digits = 0),
+      hovertxtg <- paste("infections: ", round(plotDFG$Max.N.Infections, digits = 0),
                       "<br>", "N: ", ceiling(plotDFG$N))
       theGraphG <- plotDFG %>% plot_ly(x = ~N,
                                        hoverinfo = 'text', text = hovertxtg) %>% 
-                               add_lines(y = ~Maks.L.Infekcji, color = I('#e74b47')) %>%
-                               layout(xaxis = list(title = 'Liczebność próby'),
-                                      yaxis = list(title = 'Graniczny poziom infekcji (na 1000)',
+                               add_lines(y = ~Max.N.Infections, color = I('#e74b47')) %>%
+                               layout(xaxis = list(title = 'Sample size'),
+                                      yaxis = list(title = 'Upper bound of infections (per 1000)',
                                                    tickangle = -30))
   })
   
